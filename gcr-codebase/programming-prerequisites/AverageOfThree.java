678a58ab0d455541e2178cdc4a790c6675b93d42
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-16 22:36:54 +0530

    [Vaidik Choudhary] : Added Average of Three numbers

---
+++ gcr-codebase/programming-prerequisites/AverageOfThree.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class AverageOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter second number ");
+        double b = sc.nextDouble();
+
+        System.out.print("Enter third number ");
+        double c = sc.nextDouble();
+
+        double average = (a + b + c) / 3.0;
+        System.out.println("Average = " + average);
+
+}
+}
