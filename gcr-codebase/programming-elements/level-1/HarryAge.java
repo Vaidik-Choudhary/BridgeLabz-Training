afb203febd8fa46169fc8bf8446ab6acb0df73ca
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-17 21:47:20 +0530

    [Vaidik Choudhary] : Added Harry Age

---
+++ gcr-codebase/programming-elements/level-1/HarryAge.java
@@ -0,0 +1,12 @@
+// Creating HarryAge class to compute Harry's age
+class HarryAge {
+    public static void main(String[] args) {
+
+        // Creating variables for birth year and current year
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        // Printing Harry's age
+        System.out.println("Harry's age in 2024 is " + (currentYear - birthYear));
+    }
+}
