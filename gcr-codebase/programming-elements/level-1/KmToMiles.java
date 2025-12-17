19fe5862b9ae935a2cd5825cd152f8efc131b2b5
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-17 21:53:29 +0530

    [Vaidik Choudhary] : Added KM to miles without I/o

---
+++ gcr-codebase/programming-elements/level-1/KmToMiles.java
@@ -0,0 +1,12 @@
+// Creating KmToMiles class to convert 10.8 km to miles
+class KmToMiles {
+    public static void main(String[] args) {
+
+        // Creating a variables
+        double km = 10.8;
+        double miles = km * 1.6;
+
+        // Printing the converted distance
+        System.out.println("The distance " + km + " km in miles is " + miles);
+    }
+}
