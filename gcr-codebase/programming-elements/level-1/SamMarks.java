b949c349049cff8f66f6792796b4d29a2eeb8e70
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-17 21:59:30 +0530

    [Vaidik Choudhary] : Added Sam Marks

---
+++ gcr-codebase/programming-elements/level-1/SamMarks.java
@@ -0,0 +1,13 @@
+// Creating SamMarks class to compute Sam's marks
+class SamMarks {
+    public static void main(String[] args) {
+
+        // Creating variables for Subjects
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        // Printing the Average
+        System.out.println("Sam's average mark in PCM is " + (maths + physics + chemistry) / 3.0);
+    }
+}
