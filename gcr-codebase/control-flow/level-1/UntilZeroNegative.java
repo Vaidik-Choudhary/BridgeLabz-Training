902363095491c495a8d17e0f0ef93f03d1b61128
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-18 23:45:19 +0530

    [Vaidik Choudhary] : Added UntilZeroNegative

---
+++ gcr-codebase/control-flow/level-1/UntilZeroNegative.java
@@ -0,0 +1,23 @@
+// Creating UntilZeroNegatve class to fetch zero from user
+import java.util.Scanner;
+
+class UntilZeroNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Making variables
+        double total = 0.0;
+
+        // Running While loop
+        while (true){
+            System.out.print("Enter a number : ");
+            double number = sc.nextDouble();
+            total = total + number;
+            if (number <= 0)
+                break;
+            }
+        System.out.print(total);
+
+        sc.close();
+    }
+}
