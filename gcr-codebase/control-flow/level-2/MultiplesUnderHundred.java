617868f197dbb49685118792abe74440e9596ede
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-19 02:55:24 +0530

    [Vaidik Choudhary] : Added MultiplesUnderHundred

---
+++ gcr-codebase/control-flow/level-2/MultiplesUnderHundred.java
@@ -0,0 +1,26 @@
+// Creating MultiplesUnderHundred class to find the multiples of a number under 100 
+import java.util.Scanner;
+
+class MultiplesUnderHundred {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //taking user input
+        System.out.print("Enter your number : ");
+        int number = sc.nextInt();
+
+        // checking wether the number is positive integer or not
+        if (number <= 0 || number >= 100) {
+            System.out.println("Please enter a positive under hundred number");
+        } else {
+
+                // Running for loop 
+                for (int i = 100; i >= 1 ;i--) {
+                    if (i % number == 0) {
+                        System.out.println(i);
+                    }
+                 }
+        }
+        sc.close();
+    }
+}
