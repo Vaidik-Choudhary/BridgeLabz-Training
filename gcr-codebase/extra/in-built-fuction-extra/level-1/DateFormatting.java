69fbe75e9dd72c6937faf3e52083c79a95c4fb4b
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-24 01:12:26 +0530

    [Vaidik Choudhary] : Added DateFormatting program

---
+++ gcr-codebase/extra/in-built-fuction-extra/level-1/DateFormatting.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+class DateFormatting {
+
+    // Method to display current date in different formats
+    public static void displayFormattedDates() {
+        LocalDate date = LocalDate.now();
+
+        DateTimeFormatter f1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter f2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter f3 = DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy");
+
+        System.out.println(date.format(f1));
+        System.out.println(date.format(f2));
+        System.out.println(date.format(f3));
+    }
+
+    public static void main(String[] args) {
+
+        displayFormattedDates();
+    }
+}
