dd7af1cdf81667b43c13b1601e25a11f8c8b4403
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-24 01:50:31 +0530

    [Vaidik Choudhary] : Added GCDLCM program

---
+++ gcr-codebase/extra/in-built-fuction-extra/level-2/GCDLCM.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class GCDLCM {
+
+    // Method to find GCD using Euclidean algorithm
+    public static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // Method to find LCM
+    public static int findLCM(int a, int b) {
+        return (a * b) / findGCD(a, b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("GCD: " + findGCD(a, b));
+        System.out.println("LCM: " + findLCM(a, b));
+
+        sc.close();
+    }
+}
