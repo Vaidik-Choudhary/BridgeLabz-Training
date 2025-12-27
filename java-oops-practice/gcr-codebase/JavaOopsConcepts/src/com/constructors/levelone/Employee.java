e8c76dcdd7c2561c82c7f9010667b2e422f682fd
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-27 23:52:10 +0530

    [Vaidik Choudhary] : Added Employee program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/Employee.java
@@ -0,0 +1,23 @@
+package com.constructors.levelone;
+
+public class Employee {
+
+    public int employeeID;
+    protected String department;
+    private double salary;
+
+    public Employee(int employeeID, String department, double salary) {
+        this.employeeID = employeeID;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
+
