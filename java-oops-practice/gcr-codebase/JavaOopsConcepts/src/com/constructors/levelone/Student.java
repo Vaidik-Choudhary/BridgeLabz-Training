7da244ad201b6c9fd13379409a225275007ded58
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-28 00:00:06 +0530

    [Vaidik Choudhary] : Added Student program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/Student.java
@@ -0,0 +1,23 @@
+package com.constructors.levelone;
+
+public class Student {
+
+    public int rollNumber;
+    protected String name;
+    private double cgpa;
+
+    public Student(int rollNumber, String name, double cgpa) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+}
+
