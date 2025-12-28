339582748b7ec4f9774afffa7532647eda9f6aaa
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-28 21:40:43 +0530

    [Vaidik Choudhary] : Added ValidAnagram0242 program

---
+++ leet-code-codebase/strings/ValidAnagram0242.java
@@ -0,0 +1,23 @@
+class ValidAnagram0242 {
+
+    //Method to check anagram
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int x : count) {
+            if (x != 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));
+    }
+}
