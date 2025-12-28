9c40739c5a0944f362ebc93df572fbfc45bf3508
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-28 21:29:41 +0530

    [Vaidik Choudhary] : Added ExcelColumnNumber0171 program

---
+++ leet-code-codebase/strings/ExcelColumnNumber0171.java
@@ -0,0 +1,16 @@
+class ExcelColumnNumber0171 {
+
+    //Method to convert column title to number
+    public static int titleToNumber(String s) {
+        int res = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            res = res * 26 + (s.charAt(i) - 'A' + 1);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(titleToNumber("AB"));
+    }
+}
