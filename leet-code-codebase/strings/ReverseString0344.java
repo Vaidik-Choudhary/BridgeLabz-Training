2eda7d8142ae38b20cbcfb30a856d852a12752f8
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-28 21:38:28 +0530

    [Vaidik Choudhary] : Added ReverseString0344 program

---
+++ leet-code-codebase/strings/ReverseString0344.java
@@ -0,0 +1,21 @@
+class ReverseString0344 {
+
+    //Method to reverse string
+    public static void reverseString(char[] s) {
+        int i = 0, j = s.length - 1;
+
+        while (i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        reverseString(s);
+        System.out.println(s);
+    }
+}
