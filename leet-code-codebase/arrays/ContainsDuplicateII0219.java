e9fb5867eba6446f6cee2ea2085a5e86dd9a9708
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-28 20:53:52 +0530

    [Vaidik Choudhary] : Added ContainsDuplicateII0219 program

---
+++ leet-code-codebase/arrays/ContainsDuplicateII0219.java
@@ -0,0 +1,20 @@
+class ContainsDuplicateII0219 {
+
+    //Method to check duplicates in k distance
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j] && j - i <= k) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        int k = 3;
+        System.out.println(containsNearbyDuplicate(nums, k));
+    }
+}
