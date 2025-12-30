d6cc99fae15af607ff96bcdb86a4a4aa0d53909e
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-30 22:25:42 +0530

    [Vaidik Choudhary] : Added AssignCookies0455 program

---
+++ leet-code-codebase/arrays/AssignCookies0455.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class AssignCookies0455 {
+
+    //Method to find maximum number of content children
+    public static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0;
+        int j = 0;
+        int count = 0;
+
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                count++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] g = {1,2,3};
+        int[] s = {1,1};
+
+        System.out.println(findContentChildren(g, s));
+    }
+}
