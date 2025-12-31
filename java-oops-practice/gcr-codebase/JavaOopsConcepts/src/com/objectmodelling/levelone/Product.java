8fa0842b05a72870d94aabdb541a6fbc12b7e094
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2025-12-31 14:19:08 +0530

    [Vaidik Choudhary] : Added Product program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/levelone/Product.java
@@ -0,0 +1,11 @@
+package com.objectmodelling.levelone;
+
+public class Product {
+
+    String productName;
+
+    public Product(String productName) {
+        this.productName = productName;
+    }
+}
+
