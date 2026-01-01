e176890f1a7fb55062a463c564181ad7e9956ffd
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 12:46:10 +0530

    [Vaidik Choudhary] : Added SavingsAccount.java class file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchical/bankaccounttypes/SavingsAccount.java
@@ -0,0 +1,15 @@
+package com.inheritance.hierarchical.bankaccounttypes;
+
+public class SavingsAccount extends BankAccount {
+    private double interestRate;
+
+    public SavingsAccount(String accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    public void displayAccountType() {
+        System.out.println("Account Type: Savings Account");
+    }
+}
+
