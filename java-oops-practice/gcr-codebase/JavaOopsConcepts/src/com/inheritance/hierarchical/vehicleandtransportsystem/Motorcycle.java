622353896f41f8f594fa14e6e63f2f8dd4fbb340
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 13:04:24 +0530

    [Vaidik Choudhary] : Added Motorcycle.java class file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchical/vehicleandtransportsystem/Motorcycle.java
@@ -0,0 +1,16 @@
+package com.inheritance.hierarchical.vehicleandtransportsystem;
+
+public class Motorcycle extends Vehicle {
+    private boolean hasGear;
+
+    public Motorcycle(int maxSpeed, String fuelType, boolean hasGear) {
+        super(maxSpeed, fuelType);
+        this.hasGear = hasGear;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Gear: " + hasGear);
+    }
+}
