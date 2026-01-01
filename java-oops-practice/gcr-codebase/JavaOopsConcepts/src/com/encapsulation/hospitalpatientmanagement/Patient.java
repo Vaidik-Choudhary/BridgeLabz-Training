819a74bcb6d79880a73bb2eab5d0c1ad8f635fa7
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 22:28:03 +0530

    [Vaidik Choudhary] : Added Patient.java file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/hospitalpatientmanagement/Patient.java
@@ -0,0 +1,22 @@
+package com.encapsulation.hospitalpatientmanagement;
+
+public abstract class Patient {
+    private int patientId;
+    private String name;
+    private int age;
+
+    public Patient(int id, String name, int age) {
+        this.patientId = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public abstract double calculateBill();
+
+    public void getPatientDetails() {
+        System.out.println("ID: " + patientId);
+        System.out.println("Name: " + name);
+        System.out.println("Bill: " + calculateBill());
+    }
+}
+
