6261f149d58e6c78895a057baae1821c5aea65a4
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 12:32:12 +0530

    [Vaidik Choudhary] : Added Bird.java class file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchical/animalhierarchy/Bird.java
@@ -0,0 +1,14 @@
+package com.inheritance.hierarchical.animalhierarchy;
+
+public class Bird extends Animal {
+
+    public Bird(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Bird chirps");
+    }
+}
+
