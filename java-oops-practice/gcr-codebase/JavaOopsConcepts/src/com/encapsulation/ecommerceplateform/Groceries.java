f1af35d8ab67d35fdca0d63e91d984f1e82055fe
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 22:19:15 +0530

    [Vaidik Choudhary] : Added Groceries.java file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/ecommerceplateform/Groceries.java
@@ -0,0 +1,13 @@
+package com.encapsulation.ecommerceplateform;
+
+public class Groceries extends Product {
+
+    public Groceries(int id, String name, double price) {
+        super(id, name, price);
+    }
+
+    public double calculateDiscount() {
+        return getPrice() * 0.05;
+    }
+}
+
