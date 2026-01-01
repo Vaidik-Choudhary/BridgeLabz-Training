1451172a4bfbb233ee24c86676a89b940d5e0de5
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 12:27:20 +0530

    [Vaidik Choudhary] : Added Animal.java class file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchical/animalhierarchy/Animal.java
@@ -0,0 +1,17 @@
+package com.inheritance.hierarchical.animalhierarchy;
+
+public class Animal {
+    protected String name;
+    protected int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+
