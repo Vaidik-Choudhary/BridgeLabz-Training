6b5706b6766ed8c7eaaac5694124ce780d1742a2
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 12:54:42 +0530

    [Vaidik Choudhary] : Added EmployeeDriver.java class file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchical/employeemanagementsystem/EmployeeDriver.java
@@ -0,0 +1,18 @@
+package com.inheritance.hierarchical.employeemanagementsystem;
+
+public class EmployeeDriver {
+    public static void main(String[] args) {
+
+        Employee e1 = new Manager("Rahul", 101, 80000, 10);
+        Employee e2 = new Developer("Amit", 102, 60000, "Java");
+        Employee e3 = new Intern("Neha", 103, 15000, 6);
+
+        e1.displayDetails();
+        System.out.println();
+
+        e2.displayDetails();
+        System.out.println();
+
+        e3.displayDetails();
+    }
+}
