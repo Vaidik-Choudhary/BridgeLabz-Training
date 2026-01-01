893c161bcf00d355ecfc3b3bf085b1827ea0a206
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 14:10:38 +0530

    [Vaidik Choudhary] : Added Book.java file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/single/librarymanagement/Book.java
@@ -0,0 +1,12 @@
+package com.inheritance.single.librarymanagement;
+
+public class Book {
+    protected String title;
+    protected int publicationYear;
+
+    public Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+}
+
