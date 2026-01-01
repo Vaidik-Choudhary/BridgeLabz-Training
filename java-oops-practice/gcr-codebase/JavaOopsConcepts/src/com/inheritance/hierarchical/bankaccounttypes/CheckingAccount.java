6b1e6cf42d7cc91dc596ad6ff2046148e93aabee
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 12:40:15 +0530

    [Vaidik Choudhary] : Added CheckingAccount.java class file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchical/bankaccounttypes/CheckingAccount.java
@@ -0,0 +1,15 @@
+package com.inheritance.hierarchical.bankaccounttypes;
+
+public class CheckingAccount extends BankAccount {
+    private double withdrawalLimit;
+
+    public CheckingAccount(String accountNumber, double balance, double withdrawalLimit) {
+        super(accountNumber, balance);
+        this.withdrawalLimit = withdrawalLimit;
+    }
+
+    public void displayAccountType() {
+        System.out.println("Account Type: Checking Account");
+    }
+}
+
