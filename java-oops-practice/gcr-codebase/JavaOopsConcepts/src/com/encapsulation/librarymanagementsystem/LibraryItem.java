11cfabb7edf23bee1e6d2dd96df7a3c4554a4997
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 22:33:47 +0530

    [Vaidik Choudhary] : Added LibraryItem.java file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/librarymanagementsystem/LibraryItem.java
@@ -0,0 +1,22 @@
+package com.encapsulation.librarymanagementsystem;
+
+public abstract class LibraryItem {
+    private int itemId;
+    private String title;
+    private String author;
+
+    public LibraryItem(int itemId, String title, String author) {
+        this.itemId = itemId;
+        this.title = title;
+        this.author = author;
+    }
+
+    public abstract int getLoanDuration();
+
+    public void getItemDetails() {
+        System.out.println("ID: " + itemId);
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Loan Days: " + getLoanDuration());
+    }
+}
