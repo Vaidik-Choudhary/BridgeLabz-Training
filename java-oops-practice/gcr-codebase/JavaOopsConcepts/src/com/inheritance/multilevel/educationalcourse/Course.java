2be566375472f96bd58cffcebbe5e55698e5990b
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 13:21:09 +0530

    [Vaidik Choudhary] : Added Course.java class file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevel/educationalcourse/Course.java
@@ -0,0 +1,11 @@
+package com.inheritance.multilevel.educationalcourse;
+
+public class Course {
+    protected String courseName;
+    protected int duration;
+
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+}
