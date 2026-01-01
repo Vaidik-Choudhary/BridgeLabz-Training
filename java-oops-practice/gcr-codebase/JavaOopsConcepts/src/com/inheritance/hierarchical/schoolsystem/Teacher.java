1dc73c0ba9a8051a4435ee36e121b1cbe1246a0b
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 13:01:49 +0530

    [Vaidik Choudhary] : Added Teacher.java class file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchical/schoolsystem/Teacher.java
@@ -0,0 +1,15 @@
+package com.inheritance.hierarchical.schoolsystem;
+
+public class Teacher extends Person {
+    private String subject;
+
+    public Teacher(String name, int age, String subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+
+    public void displayRole() {
+        System.out.println("Role: Teacher");
+    }
+}
+
