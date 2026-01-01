93bba2c124d6a17b4b55436267cc0803143cfebd
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 14:33:24 +0530

    [Vaidik Choudhary] : Added Thermostat.java file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/single/smarthomedevices/Thermostat.java
@@ -0,0 +1,17 @@
+package com.inheritance.single.smarthomedevices;
+
+public class Thermostat extends Device {
+    private int temperatureSetting;
+
+    public Thermostat(String deviceId, String status, int temperatureSetting) {
+        super(deviceId, status);
+        this.temperatureSetting = temperatureSetting;
+    }
+
+    public void displayStatus() {
+        System.out.println("Device ID: " + deviceId);
+        System.out.println("Status: " + status);
+        System.out.println("Temperature Setting: " + temperatureSetting + "°C");
+    }
+}
+
