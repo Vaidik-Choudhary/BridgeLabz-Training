cfa0a4c81c81488d5a0b1d9c6a701b785add7b39
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 22:18:05 +0530

    [Vaidik Choudhary] : Added Clothing.java file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/ecommerceplateform/Clothing.java
@@ -0,0 +1,13 @@
+package com.encapsulation.ecommerceplateform;
+
+public class Clothing extends Product {
+
+    public Clothing(int id, String name, double price) {
+        super(id, name, price);
+    }
+
+    public double calculateDiscount() {
+        return getPrice() * 0.20;
+    }
+}
+
