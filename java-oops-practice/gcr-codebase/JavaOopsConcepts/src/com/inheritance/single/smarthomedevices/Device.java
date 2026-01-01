112a3fbfedfc343c17234e6cffc42e64b9763c18
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 14:31:07 +0530

    [Vaidik Choudhary] : Added Device.java file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/single/smarthomedevices/Device.java
@@ -0,0 +1,11 @@
+package com.inheritance.single.smarthomedevices;
+
+public class Device {
+    protected String deviceId;
+    protected String status;
+
+    public Device(String deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+}
