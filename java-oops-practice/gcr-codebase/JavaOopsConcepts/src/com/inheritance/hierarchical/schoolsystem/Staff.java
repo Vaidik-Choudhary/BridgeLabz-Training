25ebb733aa5d5599071b6bef7dc7e7224e504746
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 12:59:30 +0530

    [Vaidik Choudhary] : Added Staff.java class file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchical/schoolsystem/Staff.java
@@ -0,0 +1,14 @@
+package com.inheritance.hierarchical.schoolsystem;
+
+public class Staff extends Person {
+    private String department;
+
+    public Staff(String name, int age, String department) {
+        super(name, age);
+        this.department = department;
+    }
+
+    public void displayRole() {
+        System.out.println("Role: Staff");
+    }
+}
