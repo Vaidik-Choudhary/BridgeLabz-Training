9fb6ff767ba9efb29a8ac708e60b689cffeb8e03
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-01 22:23:19 +0530

    [Vaidik Choudhary] : Added Employee.java file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/employeemanagementsystem/Employee.java
@@ -0,0 +1,34 @@
+package com.encapsulation.employeemanagementsystem;
+
+public abstract class Employee {
+    private int employeeId;
+    private String name;
+    private double baseSalary;
+
+    public Employee(int employeeId, String name, double baseSalary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    protected double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public abstract double calculateSalary();
+
+    public void displayDetails() {
+        System.out.println("ID: " + employeeId);
+        System.out.println("Name: " + name);
+        System.out.println("Salary: " + calculateSalary());
+    }
+}
+
