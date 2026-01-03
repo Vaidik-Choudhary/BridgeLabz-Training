5daeb996826aa6a0ab638c091ccfed3aa76e350a
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-03 16:31:30 +0530

    [Vaidik Choudhary] : Added IRideService.java File

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/cabbygo/IRideService.java
@@ -0,0 +1,12 @@
+package com.cabbygo;
+
+//Interface for ride-related operations
+public interface IRideService {
+
+ // Method to book a ride
+ void bookRide(double distance);
+
+ // Method to end a ride
+ void endRide();
+}
+
