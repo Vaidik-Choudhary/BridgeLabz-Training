63f782f00dd3f9f9f756a9bcf3cc949e6b096ec3
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-06 10:42:35 +0530

    [Vaidik Choudhary] : Added BinarySearch0704.java program

---
+++ java-oops-practice/leet-code-codebase/LeetCodeProblems/src/com/arrays/BinarySearch0704.java
@@ -0,0 +1,29 @@
+package com.arrays;
+
+class BinarySearch0704 {
+
+    //Method to search target using binary search
+    public static int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        System.out.println(search(nums, 9));
+    }
+}
+
