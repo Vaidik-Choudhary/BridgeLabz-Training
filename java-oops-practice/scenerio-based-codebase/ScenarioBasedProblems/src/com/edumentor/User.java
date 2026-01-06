665b6040e0270a5c83b1a7e3b7252030c0c32be9
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-06 22:29:24 +0530

    [Vaidik Choudhary] : Added User.java file

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/edumentor/User.java
@@ -0,0 +1,20 @@
+package com.edumentor;
+
+public abstract class User {
+
+    protected String name;
+    protected String email;
+    protected int userId;
+
+    public User(String name, String email, int userId) {
+        this.name = name;
+        this.email = email;
+        this.userId = userId;
+    }
+
+    public void displayUser() {
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("User ID: " + userId);
+    }
+}
