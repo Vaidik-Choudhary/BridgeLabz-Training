6c479e733aaeb0b72425964a331968b2cb4e9c39
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-09 11:18:03 +0530

    [Vaidik Choudhary] : Added Transport.java file

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/tourmateapplication/Transport.java
@@ -0,0 +1,15 @@
+package com.tourmateapplication;
+
+public class Transport {
+
+	private double cost;
+
+	// constructor
+	public Transport(double cost) {
+		this.cost = cost;
+	}
+
+	public double getCost() {
+		return cost;
+	}
+}
