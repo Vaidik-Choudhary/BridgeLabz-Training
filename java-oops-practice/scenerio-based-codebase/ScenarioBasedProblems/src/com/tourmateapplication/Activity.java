406c32a6eddbd65bbb5ee079c5f1bb4d3b633578
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-09 11:13:48 +0530

    [Vaidik Choudhary] : Added Activity.java file

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/tourmateapplication/Activity.java
@@ -0,0 +1,15 @@
+package com.tourmateapplication;
+
+public class Activity {
+
+    private double cost;
+
+    // constructor
+    public Activity(double cost) {
+        this.cost = cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
