77cbb6fe07b1791f390db651da6deb1874b6eb62
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-09 11:15:33 +0530

    [Vaidik Choudhary] : Added Hotel.java file

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/tourmateapplication/Hotel.java
@@ -0,0 +1,15 @@
+package com.tourmateapplication;
+
+public class Hotel {
+
+    private double cost;
+
+    // constructor
+    public Hotel(double cost) {
+        this.cost = cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
