f23c6a095ef948e48651f91e7c8109ead6cd2ab8
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-11 23:08:32 +0530

    [Vaidik Choudhary] : Added BrowserBuddy.java file

---
+++ scenerio-based-codebase/ScenarioBasedProblems/src/com/browserbuddy/BrowserBuddy.java
@@ -0,0 +1,64 @@
+package com.browserbuddy;
+
+import java.util.Stack;
+
+public class BrowserBuddy {
+
+    private class Node {
+        private String url;
+        private Node prev;
+        private Node next;
+
+        private Node(String url) {
+            this.url = url;
+        }
+    }
+
+    private Node current;
+    private Stack<String> closedTabs;
+
+    public BrowserBuddy() {
+        closedTabs = new Stack<>();
+    }
+
+    public void visit(String url) {
+        Node newNode = new Node(url);
+
+        if (current != null) {
+            current.next = newNode;
+            newNode.prev = current;
+        }
+
+        current = newNode;
+        System.out.println("Visited: " + url);
+    }
+
+    public void back() {
+        if (current != null && current.prev != null) {
+            current = current.prev;
+            System.out.println("Back to: " + current.url);
+        }
+    }
+
+    public void forward() {
+        if (current != null && current.next != null) {
+            current = current.next;
+            System.out.println("Forward to: " + current.url);
+        }
+    }
+
+    public void closeTab() {
+        if (current != null) {
+            closedTabs.push(current.url);
+            System.out.println("Closed: " + current.url);
+            current = current.prev;
+        }
+    }
+
+    public void restoreTab() {
+        if (!closedTabs.isEmpty()) {
+            visit(closedTabs.pop());
+        }
+    }
+}
+
