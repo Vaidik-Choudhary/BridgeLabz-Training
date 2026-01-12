57fd93130d7834e7b985123ce04462d5f14b91b5
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-12 11:32:13 +0530

    [Vaidik Choudhary] : Added TransposeMatrix0867 program

---
+++ java-oops-practice/leet-code-codebase/LeetCodeProblems/src/com/arrays/TransposeMatrix0867.java
@@ -0,0 +1,36 @@
+package com.arrays;
+
+class TransposeMatrix0867 {
+
+    //Method to transpose the given matrix
+    public static int[][] transpose(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] result = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int[][] result = transpose(matrix);
+
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
