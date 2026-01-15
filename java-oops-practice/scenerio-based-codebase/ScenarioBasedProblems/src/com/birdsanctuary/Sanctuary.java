b778d6fa2317aa1d8b40b64f26237e791f7b651e
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-15 15:34:30 +0530

    [Vaidik Choudhary] : Added Sanctuary.java file

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/birdsanctuary/Sanctuary.java
@@ -0,0 +1,57 @@
+package com.birdsanctuary;
+
+import java.util.ArrayList;
+
+public class Sanctuary {
+
+    private ArrayList<Bird> birds = new ArrayList<>();
+
+    public void addBird(Bird bird) {
+        birds.add(bird);
+    }
+
+    public void displayAll() {
+        for (Bird b : birds) {
+            b.display();
+        }
+    }
+
+    public void displayFlyable() {
+        for (Bird b : birds) {
+            if (b instanceof Flyable) {
+                ((Flyable) b).fly();
+            }
+        }
+    }
+
+    public void displaySwimmable() {
+        for (Bird b : birds) {
+            if (b instanceof Swimmable) {
+                ((Swimmable) b).swim();
+            }
+        }
+    }
+
+    public void removeById(String id) {
+        birds.removeIf(b -> b.getId().equals(id));
+    }
+
+    public void report() {
+        int fly = 0, swim = 0, both = 0, none = 0;
+
+        for (Bird b : birds) {
+            boolean f = b instanceof Flyable;
+            boolean s = b instanceof Swimmable;
+
+            if (f && s) both++;
+            else if (f) fly++;
+            else if (s) swim++;
+            else none++;
+        }
+
+        System.out.println("Flyable: " + fly);
+        System.out.println("Swimmable: " + swim);
+        System.out.println("Both: " + both);
+        System.out.println("Neither: " + none);
+    }
+}
