586e189e0071d5d267e4cc4fc690dee7aecf15e0
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-17 12:19:55 +0530

    [Vaidik Choudhary] : Added ClothingCategory.java file

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsProblems/src/com/generics/dynamiconlinemarketplace/ClothingCategory.java
@@ -0,0 +1,15 @@
+package com.generics.dynamiconlinemarketplace;
+
+public class ClothingCategory implements Category {
+
+    private String type;
+
+    public ClothingCategory(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String getCategoryName() {
+        return "Clothing - " + type;
+    }
+}
