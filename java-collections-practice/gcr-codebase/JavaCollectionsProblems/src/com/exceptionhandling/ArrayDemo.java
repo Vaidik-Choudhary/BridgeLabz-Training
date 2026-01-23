67cc08ba550e071b89b6d4b004c2d64d3099441c
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-23 15:01:10 +0530

    [Vaidik Choudhary] : Added ArrayDemo.java file

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsProblems/src/com/exceptionhandling/ArrayDemo.java
@@ -0,0 +1,19 @@
+package com.exceptionhandling;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30};
+
+        try {
+            int index = 5;
+            System.out.println("Value at index " + index + ": " + arr[index]);
+        } 
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index ");
+        } 
+        catch (NullPointerException e) {
+            System.out.println("Array is not initialized!");
+        }
+    }
+}
