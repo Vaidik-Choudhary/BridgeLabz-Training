24e700a61b3b06f48b8984022382a42cb89e2f05
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-26 21:30:47 +0530

    [Vaidik Choudhary] : Added ResumeAnalyzer Program

---
+++ java-collections-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/resumeanalyzer/ResumeData.java
@@ -0,0 +1,23 @@
+package com.resumeanalyzer;
+
+public class ResumeData {
+	String email;
+	String phone;
+	int matchCount;
+	String fileName;
+
+	public ResumeData(String email, String phone, int matchCount, String fileName) {
+		this.email = email;
+		this.phone = phone;
+		this.matchCount = matchCount;
+		this.fileName = fileName;
+	}
+
+	public String toString() {
+		return email + " | " + phone + " | " + matchCount + "      | " + fileName;
+	}
+
+	public int getMatchCount() {
+		return matchCount;
+	}
+}
