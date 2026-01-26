373a8d12600b599884ce28f47354058b3a57fe43
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-26 22:16:25 +0530

    [Vaidik Choudhary] : Added Intermediate Level Custom Annotations Programs

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsProblems/src/com/customannotations/intermediatelevel/loggingmethodexecution/LogExecutionTime.java
@@ -0,0 +1,7 @@
+package com.customannotations.intermediatelevel.loggingmethodexecution;
+
+
+import java.lang.annotation.*;
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface LogExecutionTime {}
