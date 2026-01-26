de173805aef1aca601595405c36602dcfdaa0914
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-26 22:07:42 +0530

    [Vaidik Choudhary] : Added RepeatableAnotation Program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsProblems/src/com/annotations/repeatableannotation/BugTracker.java
@@ -0,0 +1,9 @@
+package com.annotations.repeatableannotation;
+
+public class BugTracker {
+
+    @BugReport(description = "NullPointerException")
+    @BugReport(description = "Performance issue")
+    void process() {}
+    
+}
