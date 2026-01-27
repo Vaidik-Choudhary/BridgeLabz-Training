c2d94b41a00955fda7563d0f9065b0b8ec8b6744
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-27 14:58:49 +0530

    [Vaidik Choudhary] : Delete java-collections-practice/gcr-codebase/JavaCollectionsProblems/src/com/junit directory

--- java-collections-practice/gcr-codebase/JavaCollectionsProblems/src/com/junit/testingparameterizedtests/NumberUtils.java
+++
@@ -1,9 +0,0 @@
-package com.junit.testingparameterizedtests;
-
-public class NumberUtils {
-
-    // Returns true if number is even
-    public boolean isEven(int number) {
-        return number % 2 == 0;
-    }
-}
