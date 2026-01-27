cdba49c437bc5896ac4c5ddfd8d10ceda24ac8d3
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-01-27 15:55:14 +0530

    [Vaidik Choudhary] : Added testingparameterizedtests Main file

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsProblems/src/com/junit/main/testingparameterizedtests/NumberUtils.java
@@ -0,0 +1,14 @@
+package com.junit.main.testingparameterizedtests;
+
+/*
+ * NumberUtils
+ * -----------
+ * Utility class for number-related operations.
+ */
+public class NumberUtils {
+
+    // Returns true if number is even
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
