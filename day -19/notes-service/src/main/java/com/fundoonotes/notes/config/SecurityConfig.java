fd4d608c57c9bc6904e746168039df08e7de54c5
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-08-27 12:09:39 +0530

    [Vaidik Choudhary] : Updated the Fundoo Notes App, Added Admin ser

--- day -19/notes-service/src/main/java/com/fundoonotes/notes/config/SecurityConfig.java
+++ day -19/notes-service/src/main/java/com/fundoonotes/notes/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
 		http.csrf(csrf -> csrf.disable())
 		    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 		    .authorizeHttpRequests(auth -> auth
+				.requestMatchers("/actuator/**").permitAll()
 				.anyRequest()
 				.authenticated()
 		    	)
